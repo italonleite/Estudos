a1ea12065c480b8db777e80eecf579fa995afc3e
Italo Nunes Leite <italonleite@gmail.com>
2021-05-22 22:04:31 -0300

    mapeamento immobile

---
+++ ConsoleEFDescriminator/Map/ImmobileMap.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleEFDescriminator.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleEFDescriminator.Map
+{
+    public class ImmobileMap : IEntityTypeConfiguration<Immobile>
+    {
+        public void Configure(EntityTypeBuilder<Immobile> builder)
+        {
+            builder
+              .HasDiscriminator<string>("imovel_descriminator_tipo")
+              .HasValue<Residential>("residencial")
+              .HasValue<Commercial>("comercial");
+              builder.HasKey(x => x.ImmobileId);
+        }
+      
+    }
+}
