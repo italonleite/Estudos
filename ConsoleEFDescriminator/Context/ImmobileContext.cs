8edf5569c361be25585f0fc41e0ac45c57f12394
Italo Nunes Leite <italonleite@gmail.com>
2021-05-22 21:50:21 -0300

    criando dbContext

---
+++ ConsoleEFDescriminator/Context/ImmobileContext.cs
@@ -0,0 +1,27 @@
+﻿using ConsoleEFDescriminator.Map;
+using ConsoleEFDescriminator.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleEFDescriminator.Context
+{
+    public class ImmobileContext : DbContext
+    {
+        public DbSet<Immobile> Immobiles { get; set; }
+        public DbSet<Commercial> Commercials { get; set; }
+        public DbSet<Residential> Residentials { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=ITALO\SQLNOVO; Database=EFDiscriminator;Trusted_Connection=True; MultipleActiveResultSets=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ImmobileMap());
+        }
+  
+    }
+}
