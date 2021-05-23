f22a4529c8c7c48184885f46693eacfec230ecb0
Italo Nunes Leite <italonleite@gmail.com>
2021-05-22 22:04:55 -0300

    criando migration v1

---
+++ ConsoleEFDescriminator/Migrations/20210523002545_v1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ConsoleEFDescriminator.Migrations
+{
+    public partial class v1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Immobiles",
+                columns: table => new
+                {
+                    ImmobileId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    imovel_descriminator_tipo = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Immobiles", x => x.ImmobileId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Immobiles");
+        }
+    }
+}
